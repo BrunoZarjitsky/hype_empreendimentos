8044485494d59a805b13ded6cd2144be7c2cd6c6
brunozarjitsky <brunozar@poli.ufrj.br>
2023-02-24 09:30:16 -0300

    created basic endpoint

--- HBM_plus/views.py
+++ HBM_plus/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import status
 
-# Create your views here.
+class HeartBeatMonitorPlusViewSet(ViewSet):
+    @action(detail=False, methods=["GET"], url_path="read_heart_beat_monitor")
+    def get_heart_beat_monitor(self, request):
+        try:
+            return Response(
+                data = {}, 
+                status = status.HTTP_200_OK,
+            )
+        except Exception as error:
+            return Response(
+                data = {
+                    "message": "Unexpected error",
+                    "detail": str(error),
+                }, 
+                status = status.HTTP_400_BAD_REQUEST,
+            )
