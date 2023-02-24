4f8bd66b6f10398a73bf9a153e48f05e0f32ef50
brunozarjitsky <brunozar@poli.ufrj.br>
2023-02-24 10:17:40 -0300

    added the value for the endpoint response

--- HBM_plus/views.py
+++ HBM_plus/views.py
@@ -2,20 +2,25 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import status
+from HBM_plus.managers.HBMP_manager import HeartBeatMonitorPlusManager
+
 
 class HeartBeatMonitorPlusViewSet(ViewSet):
     @action(detail=False, methods=["GET"], url_path="read_heart_beat_monitor")
     def get_heart_beat_monitor(self, request):
         try:
+            manager = HeartBeatMonitorPlusManager()
             return Response(
-                data = {}, 
-                status = status.HTTP_200_OK,
+                data={
+                    "value": manager.read_monitor(),
+                },
+                status=status.HTTP_200_OK,
             )
         except Exception as error:
             return Response(
-                data = {
+                data={
                     "message": "Unexpected error",
                     "detail": str(error),
-                }, 
-                status = status.HTTP_400_BAD_REQUEST,
+                },
+                status=status.HTTP_400_BAD_REQUEST,
             )
