f69f3bfddabec243ea00648b7ce49884c9a737f1
brunozarjitsky <brunozar@poli.ufrj.br>
2023-02-24 14:44:21 -0300

    created endpoint for stress test

--- HBM_plus/views.py
+++ HBM_plus/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import status
 from HBM_plus.managers.HBMP_manager import HeartBeatMonitorPlusManager
+from HBM_plus.scripts.stress_test_api import StressTestManager
+from time import time
 
 
 class HeartBeatMonitorPlusViewSet(ViewSet):
@@ -21,6 +23,29 @@ class HeartBeatMonitorPlusViewSet(ViewSet):
                 data={
                     "message": "Unexpected error",
                     "detail": str(error),
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+
+class StressTestViewSet(ViewSet):
+    @action(detail=False, methods=["GET"], url_path="run_stress_test")
+    def get_stress_test_results(self, request):
+        try:
+            initial_time = time()
+            time_testing_in_seconds = int(request.data.get(
+                "time_testing_in_seconds", 1))
+            manager = StressTestManager(time_testing_in_seconds)
+            return Response(
+                data=manager.get_stress_test_results(),
+                status=status.HTTP_200_OK,
+            )
+        except Exception as error:
+            return Response(
+                data={
+                    "message": "Unexpected error",
+                    "detail": str(error),
+                    "time_runned": time() - initial_time,
                 },
                 status=status.HTTP_400_BAD_REQUEST,
             )
--- hype_empreendimentos/urls.py
+++ hype_empreendimentos/urls.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from HBM_plus.views import HeartBeatMonitorPlusViewSet
+from HBM_plus.views import (
+    HeartBeatMonitorPlusViewSet,
+    StressTestViewSet,
+)
 
 HBM_plus_router = routers.DefaultRouter()
 HBM_plus_router.register(
     "HBM_plus", HeartBeatMonitorPlusViewSet, basename="Heart beat monitor plus")
 
+stress_test_router = routers.DefaultRouter()
+stress_test_router.register(
+    "stress_test", StressTestViewSet, basename="stress_test_router"
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(HBM_plus_router.urls)),
+    path('', include(stress_test_router.urls)),
 ]
