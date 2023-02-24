ad500fadb1c898257672235aed835dcca6f0cf89
brunozarjitsky <brunozar@poli.ufrj.br>
2023-02-24 10:18:15 -0300

    typed the return of the methods from HeartBeatMonitorPlusManager

--- HBM_plus/managers/HBMP_manager.py
+++ HBM_plus/managers/HBMP_manager.py
@@ -7,12 +7,12 @@ class HeartBeatMonitorPlusManager:
         self.monitor_equation = "-0.06366 + 0.12613 * cos(pi*timestamp/500) + 0.12258 * cos(pi*timestamp/250)"\
             " + 0.01593 * sin(pi*timestamp/500) + 0.03147 * sin(pi*timestamp/250)"
 
-    def read_monitor(self):
+    def read_monitor(self) -> float:
         current_timestamp = self._get_current_timestamp()
         return self._calculate_HBMP(current_timestamp)
 
     @staticmethod
-    def _get_current_timestamp():
+    def _get_current_timestamp() -> int:
         return trunc(timezone.now().timestamp() * 1000)
 
     def _calculate_HBMP(self, timestamp: float) -> float:
